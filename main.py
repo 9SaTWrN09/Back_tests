e113f14f2af539c042e369b9636f3c552a08c290
Emmanuel Lopez Ramirez <emmanuel.lopezr@udea.edu.co>
2025-02-12 13:22:56 -0500

    Prueba 2 de subir el main corregido

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import uvicorn
+import os
 
 def start():
+    port = int(os.getenv("PORT", 8000))  # Usa el puerto que asigna Railway o 8000 por defecto
     uvicorn.run("Aplication.APiTest:app", 
-                host="127.0.0.1", 
-                port=8030, 
+                host="0.0.0.0",  # Permite accesos externos
+                port=port, 
                 reload=True
                 )
-    
+
 if __name__ == "__main__":
     start()
+
